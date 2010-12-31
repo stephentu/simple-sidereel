73ea889e3c96c7ea6162d727b91f6a443f1ff309
Stephen Tu <stephen_tu@berkeley.edu>
2010-12-31 13:42:50 -0800

    add parallelism

--- src/main/java/com/stephentu/SimpleSidereel.java
+++ src/main/java/com/stephentu/SimpleSidereel.java
@@ -1,7 +1,13 @@
 package com.stephentu;
 
+import java.io.IOException;
+
 import java.util.ArrayList;
 import java.util.List;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -10,17 +16,72 @@ import org.jsoup.select.Elements;
 
 public class SimpleSidereel {
 
+  static class LinkTask implements Runnable {
+    private final List<String> linksToFollow;
+    private final CountDownLatch latch;
+    LinkTask(List<String> linksToFollow) {
+      this(linksToFollow, null);
+    }
+    LinkTask(List<String> linksToFollow, CountDownLatch latch) {
+      this.linksToFollow = linksToFollow;
+      this.latch = latch;
+    }
+    private void doRun() throws IOException {
+      for (String link : linksToFollow) {
+        Document innerDoc; 
+        if (link.startsWith("/"))
+          innerDoc = Jsoup.connect("http://www.sidereel.com" + link).get();
+        else if (link.startsWith("http"))
+          innerDoc = Jsoup.connect(link).get();
+        else {
+          System.err.println("cannot handle link: " + link); 
+          continue;
+        }
+        Element trueLink = innerDoc.select("div[class^=playground] strong a").first();
+        if (trueLink != null) {
+          String trueURL = trueLink.attr("href");
+          System.out.println(trueURL);
+        } else 
+          System.out.println("Bad redirect: " + link);
+      }
+      if (latch != null)
+        latch.countDown();
+    }
+    public void run() {
+      try { doRun(); } 
+      catch (IOException e) {
+        throw new RuntimeException(e);
+      }
+    }
+  }
+
+  /** 
+   * does idx lie within [start, end]
+   */
+  private static <T> boolean checkBounds(List<T> l, int idx) {
+    return idx >= 0 && idx <= l.size();
+  }
+
+  /**
+   * Return a slice from [start, end) of orig
+   */
+  private static <T> List<T> sliceOf(List<T> orig, int start, int end) {
+    if (!checkBounds(orig, start) || !checkBounds(orig, end))
+      throw new IllegalArgumentException("bad index given");
+    List<T> cpy = new ArrayList<T>();
+    for (int idx = start; idx < end; idx++)
+      cpy.add(orig.get(idx));
+    return cpy;
+  }
+
   public static void main(String[] args) throws Exception {
-    
-    //String showName = "The_Big_Bang_Theory";
-    //int season = 22;
-    //int episode = 1;
     
     String showName;
     int season, episode;
+    boolean forceNoParallel = false;
     
     if (args.length < 3) {
-      System.err.println("Usage: java com.stephentu.SimpleSidereel [show name] [season] [episode]");
+      System.err.println("Usage: java com.stephentu.SimpleSidereel [show name] [season] [episode] [--noparallel]");
       System.exit(1);
     }
     
@@ -31,6 +92,11 @@ public class SimpleSidereel {
     if (season <= 0 || episode <= 0) {
       System.err.println("Season and episode numbers must be >= 1");
       System.exit(2);
+    }
+
+    if (args.length >= 4 && args[3].equals("--noparallel")) {
+      System.out.println("Disabling parallelism");
+      forceNoParallel = true;
     }
     
     System.out.println("----------------------------------");
@@ -46,7 +112,6 @@ public class SimpleSidereel {
     
     Elements pagination = doc.select("div[class=pagination]");
     if (!pagination.isEmpty()) {
-      //System.out.println(pagination);
       Elements pageNums = pagination.select("span:matchesOwn(\\d+),a:matchesOwn(\\d+)");
       if (!pageNums.isEmpty()) {
         int maxSoFar = 1;
@@ -59,24 +124,34 @@ public class SimpleSidereel {
       }
     }
     
-    boolean emittedLinks = false;
+    List<String> linksToFollow = new ArrayList<String>();
     for (Document docToCheck : docsToCheck) {
       Elements links = docToCheck.select("li[class^=link_] h2 a");
-      //System.out.println(links);
       for (Element elem : links) {
-        //System.out.println("Accessing: " + elem.attr("href"));
-        Document innerDoc = Jsoup.connect("http://www.sidereel.com" + elem.attr("href")).get();
-        Element trueLink = innerDoc.select("div[class^=playground] strong a").first();
-        emittedLinks = true;
-        if (trueLink != null) {
-          String trueURL = trueLink.attr("href");
-          System.out.println(trueURL);
-        } else 
-          System.out.println("Bad redirect: " + elem.attr("href"));
-        //break;
+        linksToFollow.add(elem.attr("href"));
       }
     }
-    if (!emittedLinks)
+    if (linksToFollow.isEmpty())
       System.out.println("Sorry, could not find any links");
+    else {
+      int numProc = Runtime.getRuntime().availableProcessors();
+      int numThreads = numProc * 2;
+      if (!forceNoParallel && linksToFollow.size() >= numThreads) {
+        // execute on numProc threads
+        ExecutorService exec = Executors.newFixedThreadPool(numThreads);
+        CountDownLatch latch = new CountDownLatch(numThreads);
+        int linksPerThread = linksToFollow.size() / numThreads;
+        int lastIdx = 0;
+        for (int i = 0; i < numThreads - 1; i++) {
+          int endIdx = lastIdx + linksPerThread;
+          exec.execute(new LinkTask(sliceOf(linksToFollow, lastIdx, endIdx), latch));
+          lastIdx = endIdx;
+        }
+        exec.execute(new LinkTask(sliceOf(linksToFollow, lastIdx, linksToFollow.size()), latch));
+        latch.await();
+        exec.shutdown();
+      } else
+        new LinkTask(linksToFollow).run();
+    }
   }
 }
