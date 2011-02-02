d836845a3d9b7839d984c82b2c4cdc99a2e1f46b
Stephen Tu <stephen_tu@berkeley.edu>
2011-02-01 23:11:06 -0800

    fetch season summaries in parallel now

--- src/main/java/com/stephentu/SimpleSidereel.java
+++ src/main/java/com/stephentu/SimpleSidereel.java
@@ -4,8 +4,11 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.SortedMap;
 import java.util.TreeMap;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -24,7 +27,9 @@ import com.stephentu.util.Tuple2;
 public class SimpleSidereel {
   
   private static final int TIMEOUT = 5000;
-
+  private static final int numProc = Runtime.getRuntime().availableProcessors();
+  private static final int numThreads = numProc * 2;
+  
   static class LinkTask implements Runnable {
     private final List<String> linksToFollow;
     private final CountDownLatch latch;
@@ -125,8 +130,6 @@ public class SimpleSidereel {
     if (linksToFollow.isEmpty())
       System.out.println("Sorry, could not find any links");
     else {
-      int numProc = Runtime.getRuntime().availableProcessors();
-      int numThreads = numProc * 2;
       if (!forceNoParallel && linksToFollow.size() >= numThreads) {
         // execute on numProc threads
         ExecutorService exec = Executors.newFixedThreadPool(numThreads);
@@ -149,7 +152,61 @@ public class SimpleSidereel {
   private static final Pattern seasonUrlPattern = Pattern.compile("/\\w+/season-(\\d+)");
   private static final Pattern episodeUrlPattern = Pattern.compile("/\\w+/season-\\d+/episode-(\\d+)");
   
-  private static SortedMap<Integer, SortedMap<Integer, Tuple2<Element, String>>> retrieveEpisodeSummaries(String showName, Function1<Integer, Boolean> seasonInclusionCriteria) throws IOException {
+  private static <T> List<T> setToList(Set<T> s) {
+    List<T> l = new ArrayList<T>(s.size());
+    for (T t : s) l.add(t);
+    return l;
+  }
+  
+  private static <T> List<T> allocate(int size) {
+    List<T> l = new ArrayList<T>(size);
+    for (int i = 0; i < size; i++)
+      l.add(null);
+    return l;
+  }
+  
+  private static class SeasonEpisodeWorker implements Runnable {
+
+    private final int seasonIdx;
+    private final Element elem;
+    private final BlockingQueue<Tuple2<Integer, SortedMap<Integer, Tuple2<Element, String>>>> answerQueue;
+    
+    SeasonEpisodeWorker(int seasonIdx, Element elem, BlockingQueue<Tuple2<Integer, SortedMap<Integer, Tuple2<Element, String>>>> answerQueue) {
+      this.seasonIdx = seasonIdx;
+      this.elem = elem;
+      this.answerQueue = answerQueue;
+    }
+    
+    private void doRun() throws IOException, InterruptedException {
+      String seasonFragmentUrl = "http://www.sidereel.com" + elem.attr("data-load-from");
+      Document seasonDoc = connectionWithTimeout(seasonFragmentUrl, TIMEOUT).get();
+      Elements episodes = seasonDoc.select("div[class^=episode-header] a[class^=expand-episode-trigger]");
+      SortedMap<Integer, Tuple2<Element, String>> episodeMap = new TreeMap<Integer, Tuple2<Element, String>>();
+      for (Element e : episodes) {
+        Matcher m = episodeUrlPattern.matcher(e.attr("href"));
+        if (!m.matches())
+          throw new RuntimeException("Found invalid URL from episode element: " + e.attr("href"));
+        int episodeNumber = Integer.parseInt(m.group(1));
+        String episodeFragmentUrl = "http://www.sidereel.com" + e.attr("data-load-from");
+        Document episodeDoc = connectionWithTimeout(episodeFragmentUrl, TIMEOUT).get();
+        Elements summaries = episodeDoc.select("div[class^=episode-summary]");
+        episodeMap.put(episodeNumber, new Tuple2<Element, String>(e, summaries.first().text()));
+      }
+      answerQueue.put(new Tuple2<Integer, SortedMap<Integer, Tuple2<Element, String>>>(seasonIdx, episodeMap));
+    }
+
+    @Override
+    public void run() {
+      try { doRun(); } catch (IOException e) {
+        throw new RuntimeException(e);
+      } catch (InterruptedException e) {
+        throw new RuntimeException(e);
+      }
+    }
+    
+  }
+  
+  private static void retrieveEpisodeSummaries(String showName, Function1<Integer, Boolean> seasonInclusionCriteria) throws IOException, InterruptedException {
     
     Document doc = connectionWithTimeout(String.format("http://www.sidereel.com/%s", showName), TIMEOUT).get();
 
@@ -166,52 +223,56 @@ public class SimpleSidereel {
         seasonMap.put(seasonNumber, e);
     }
     
-    // (season) -> (episode -> (element for this <season, episode>, episode description))
-    SortedMap<Integer, SortedMap<Integer, Tuple2<Element, String>>> seasonEpisodeMap = new TreeMap<Integer, SortedMap<Integer, Tuple2<Element, String>>>();
-    // TODO: parallelize
-    for (Map.Entry<Integer, Element> entry : seasonMap.entrySet()) {
-      String seasonFragmentUrl = "http://www.sidereel.com" + entry.getValue().attr("data-load-from");
-      Document seasonDoc = connectionWithTimeout(seasonFragmentUrl, TIMEOUT).get();
-      Elements episodes = seasonDoc.select("div[class^=episode-header] a[class^=expand-episode-trigger]");
-      SortedMap<Integer, Tuple2<Element, String>> episodeMap = new TreeMap<Integer, Tuple2<Element, String>>();
-      for (Element e : episodes) {
-        Matcher m = episodeUrlPattern.matcher(e.attr("href"));
-        if (!m.matches())
-          throw new RuntimeException("Found invalid URL from episode element: " + e.attr("href"));
-        int episodeNumber = Integer.parseInt(m.group(1));
-        String episodeFragmentUrl = "http://www.sidereel.com" + e.attr("data-load-from");
-        Document episodeDoc = connectionWithTimeout(episodeFragmentUrl, TIMEOUT).get();
-        Elements summaries = episodeDoc.select("div[class^=episode-summary]");
-        episodeMap.put(episodeNumber, new Tuple2<Element, String>(e, summaries.first().text()));
-      }
-      seasonEpisodeMap.put(entry.getKey(), episodeMap);
+    if (seasonMap.isEmpty()) {
+      System.out.println("No seasons found");
+      return;
     }
+     
+    List<Integer> seasonList = setToList(seasonMap.keySet()); // already in sorted order
+    List<SortedMap<Integer, Tuple2<Element, String>>> seasonBuffer = allocate(seasonList.size()); // prefills with null values
 
-    return seasonEpisodeMap;
+    BlockingQueue<Tuple2<Integer, SortedMap<Integer, Tuple2<Element, String>>>> workQueue = new ArrayBlockingQueue<Tuple2<Integer, SortedMap<Integer, Tuple2<Element, String>>>>(seasonList.size());
+    ExecutorService exec = Executors.newFixedThreadPool(numThreads);
+    
+    for (int i = 0; i < seasonList.size(); i++)
+      exec.submit(new SeasonEpisodeWorker(i, seasonMap.get(seasonList.get(i)), workQueue));
+
+    int curIdx = 0;
+    while (curIdx < seasonList.size()) {
+      SortedMap<Integer, Tuple2<Element, String>> curSeason = seasonBuffer.get(curIdx);
+      if (curSeason == null) {
+        Tuple2<Integer, SortedMap<Integer, Tuple2<Element, String>>> result = workQueue.take();
+        seasonBuffer.set(result._1, result._2);
+      } else {
+        printEpisodeSummaries(seasonList.get(curIdx), curSeason);
+        curIdx++;
+      }
+    }
+    
+    exec.shutdownNow();
   }
   
-  private static void printEpisodeSummaries(SortedMap<Integer, SortedMap<Integer, Tuple2<Element, String>>> summaries) {
-    for (Map.Entry<Integer, SortedMap<Integer, Tuple2<Element, String>>> e0 : summaries.entrySet()) {
-      System.out.println(String.format("==== Season %d ====", e0.getKey()));
-      int maxLen = e0.getValue().lastKey().toString().length();
-      for (Map.Entry<Integer, Tuple2<Element, String>> e1 : e0.getValue().entrySet()) {
-        System.out.println(String.format("Episode %" + maxLen + "d: %s", e1.getKey(), e1.getValue()._2));
-      }
+  private static void printEpisodeSummaries(int season, SortedMap<Integer, Tuple2<Element, String>> summaries) {
+    System.out.println(String.format("==== Season %d ====", season));
+    int maxLen = summaries.lastKey().toString().length();
+    for (Map.Entry<Integer, Tuple2<Element, String>> e1 : summaries.entrySet()) {
+      System.out.println(String.format("Episode %" + maxLen + "d: %s", e1.getKey(), e1.getValue()._2));
     }
   }
   
-  private static void episodeSummaries(String showName) throws IOException {
-    printEpisodeSummaries(retrieveEpisodeSummaries(showName, new Function1<Integer, Boolean>() {
+  
+  private static void episodeSummaries(String showName) throws IOException, InterruptedException {
+    retrieveEpisodeSummaries(showName, new Function1<Integer, Boolean>() {
       @Override
       public Boolean apply(Integer a0) { return true; }
-    }));
+    });
   }
   
-  private static void episodeSummariesForSeason(String showName, final int season) throws IOException {
-    printEpisodeSummaries(retrieveEpisodeSummaries(showName, new Function1<Integer, Boolean>() {
+  private static void episodeSummariesForSeason(String showName, final int season) throws IOException, InterruptedException {
+   retrieveEpisodeSummaries(showName, new Function1<Integer, Boolean>() {
       @Override
       public Boolean apply(Integer a0) { return season == a0; }
-    }));
+    });
   }
   
   private static void printUsageAndExit(int code) {
